f5bd5b85fdb5168e8a9efb6e995874e101d9a50c
Maksym Honchar <maksim.honchar@gmail.com>
2020-09-22 23:17:09 +0300

    add TopBar

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { ThemeProvider } from '@material-ui/core/styles';
 import App from './App';
-import theme from './theme';
+
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+  <React.Fragment>
     <CssBaseline />
     <App />
-  </ThemeProvider>,
-  document.querySelector('#root'),
+  </React.Fragment>,
+  document.querySelector('#root')
 );
