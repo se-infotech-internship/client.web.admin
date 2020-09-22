66f9f51bc2c119aca2fd95dd3c6917ccc98ee72d
Maksym Honchar <maksim.honchar@gmail.com>
2020-09-23 00:25:11 +0300

    add table

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import TopBar from './components/TopBar';
+import BasicTable from './components/Table';
 
 
 export default function App() {
   return (
     <React.Fragment>
       <TopBar />
+      <BasicTable />
     </React.Fragment>
   );
 }
