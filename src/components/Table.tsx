82349edbe265efde07fc0e0717fce67707670569
Maksym Honchar <maksim.honchar@gmail.com>
2020-09-23 11:38:26 +0300

    change search topbar

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -14,16 +14,16 @@ const useStyles = makeStyles({
     },
 });
 
-function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
-    return { name, calories, fat, carbs, protein };
+function createData(vehicleNumber: string, userName: string, fines: number, appPayment: string) {
+    return { vehicleNumber, userName, fines, appPayment };
 }
 
 const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('АА 0000 КК', 'Петренюк Семен Александрович', 6, 'Так'),
+    createData('АА 0000 КК', 'Петренюк Семен Александрович', 6, 'Так'),
+    createData('АА 0000 КК', 'Петренюк Семен Александрович', 6, 'Так'),
+    createData('АА 0000 КК', 'Петренюк Семен Александрович', 6, 'Так'),
+    createData('АА 0000 КК', 'Петренюк Семен Александрович', 6, 'Так')
 ];
 
 export default function BasicTable() {
@@ -34,23 +34,21 @@ export default function BasicTable() {
             <Table className={classes.table} aria-label="simple table">
                 <TableHead>
                     <TableRow>
-                        <TableCell>Dessert (100g serving)</TableCell>
-                        <TableCell align="right">Calories</TableCell>
-                        <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                        <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                        <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                        <TableCell>Номер транспортного засобу</TableCell>
+                        <TableCell align="center">Користувач</TableCell>
+                        <TableCell align="center">Штрафи</TableCell>
+                        <TableCell align="center">Оплата додатку</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {rows.map((row) => (
-                        <TableRow key={row.name}>
+                        <TableRow key={row.vehicleNumber}>
                             <TableCell component="th" scope="row">
-                                {row.name}
+                                {row.vehicleNumber}
                             </TableCell>
-                            <TableCell align="right">{row.calories}</TableCell>
-                            <TableCell align="right">{row.fat}</TableCell>
-                            <TableCell align="right">{row.carbs}</TableCell>
-                            <TableCell align="right">{row.protein}</TableCell>
+                            <TableCell align="center">{row.userName}</TableCell>
+                            <TableCell align="center">{row.fines}</TableCell>
+                            <TableCell align="center">{row.appPayment}</TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
