9c4a66b645ca7a0f4e18cf09155d967317fd7f9e
Maksym Honchar <maksim.honchar@gmail.com>
2020-09-29 12:10:42 +0300

    add redux store and slice

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import App from "./App";
 
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
-  <React.Fragment>
+  <Provider store={store}>
     <CssBaseline />
     <App />
-  </React.Fragment>,
-  document.querySelector('#root')
+  </Provider>,
+  document.querySelector("#root")
 );
---
+++ src/redux/usersSlice.ts
@@ -0,0 +1,42 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "./store";
+
+let fakeUsers = [
+  {
+    id: "123",
+    name: "Louis N. Thomas",
+    mail: "thomas@gmail.com",
+    phone: "+380934562312",
+    vehNum: "AA 94534 KK",
+    status: true,
+    photo: "https://bold.textcontrol.com/images/authors/Bjoern%20Meyer.jpg",
+  },
+  {
+    id: "345",
+    name: "Martina L. Maynes",
+    mail: "martin@gmail.com",
+    phone: "+380354562185",
+    vehNum: "HA 18426 TH",
+    status: false,
+    photo:
+      "https://cdn.pixabay.com/photo/2018/04/04/10/11/portrait-3289372_960_720.jpg",
+  },
+];
+
+export const usersSlice = createSlice({
+  name: "users",
+  initialState: {
+    value: fakeUsers,
+  },
+  reducers: {
+    deleteUser: (state, action) => {
+      state.value = state.value.filter((user) => user.id !== action.payload);
+    },
+  },
+});
+
+export const { deleteUser } = usersSlice.actions;
+
+export const selectUsers = (state: RootState) => state.value;
+
+export default usersSlice.reducer;
