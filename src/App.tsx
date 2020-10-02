ea5396df05dc6611be1b5f6afcad022302a5e40b
Maksym Honchar <maksim.honchar@gmail.com>
2020-10-02 18:31:36 +0300

    home commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React from "react"
 
-import SignIn from "./components/SignIn";
-import TopBar from "./components/TopBar";
-import ListUsers from "./components/ListUsers";
+import SignIn from "./components/SignIn"
+import TopBar from "./components/TopBar"
+import ListUsers from "./components/ListUsers"
 
 export default function App() {
-  const [sign, setSign] = React.useState(true);
+  const [sign, setSign] = React.useState(true)
 
-  const loginScreen = <SignIn />;
+  const loginScreen = <SignIn />
 
   const adminScreen = (
     <React.Fragment>
@@ -16,5 +16,5 @@ export default function App() {
     </React.Fragment>
   );
 
-  return <React.Fragment>{sign ? adminScreen : loginScreen}</React.Fragment>;
+  return <React.Fragment>{sign ? adminScreen : loginScreen}</React.Fragment>
 }
