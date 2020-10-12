6031fa56d7b3996cb2e9359871038add27048203
Maksym Honchar <maksim.honchar@gmail.com>
2020-10-12 11:55:18 +0300

    save changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import LoginPage from './components/LoginPage'
 import { HomePage } from './components/homepage/HomePage'
 import { PrivateRoute } from './PrivateRoute'
+import { history } from './helpers/history'
 
 const token = window.localStorage.getItem('token')
 
@@ -12,13 +13,13 @@ export default function App() {
 
   return (
     <React.Fragment>
-      {/* <Router history={history}>
+      <Router history={history}>
         <div>
           <PrivateRoute exact path="/" component={HomePage} />
           <Route path="/login" component={LoginPage} />
         </div>
-      </Router> */}
-      <LoginPage />
+      </Router>
+      {/* <LoginPage /> */}
       {/* <HomePage /> */}
     </React.Fragment>
   )
--- src/PrivateRoute.tsx
+++ src/PrivateRoute.tsx
@@ -3,8 +3,10 @@ import { Route, Redirect } from 'react-router-dom';
 
 export const PrivateRoute = ({ component: Component, ...rest }: { component: any }) => (
     <Route {...rest} render={props => (
-        localStorage.getItem('user')
+        localStorage.getItem('token')
             ? <Component {...props} />
             : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
     )} />
 )
+
+
