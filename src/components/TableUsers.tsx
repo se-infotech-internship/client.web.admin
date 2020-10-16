685e8f7eba9056991249719bc2b18a789ff80b82
Maksym Honchar <maksim.honchar@gmail.com>
2020-10-16 23:01:17 +0300

    refact table

--- src/components/TableUsers.tsx
+++ src/components/TableUsers.tsx
@@ -10,7 +10,7 @@ import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 
 interface Column {
-    id: 'name' | 'code' | 'population' | 'size' | 'density';
+    id: 'name' | 'code' | 'population' | 'size';
     label: string;
     minWidth?: number;
     align?: 'right';
@@ -34,44 +34,55 @@ const columns: Column[] = [
         align: 'right',
         format: (value: number) => value.toLocaleString('en-US'),
     },
-    {
-        id: 'density',
-        label: 'Density',
-        minWidth: 170,
-        align: 'right',
-        format: (value: number) => value.toFixed(2),
-    },
+    // {
+    //     // id: 'density',
+    //     label: 'Density',
+    //     minWidth: 170,
+    //     align: 'right',
+    //     format: (value: number) => value.toFixed(2),
+    // },
 ];
 
-interface Data {
-    name: string;
-    code: string;
-    population: number;
-    size: number;
-    density: number;
-}
+// interface Data {
+//     name: string;
+//     code: string;
+//     population: number;
+//     size: number;
+//     density: number;
+// }
 
-function createData(name: string, code: string, population: number, size: number): Data {
-    const density = population / size;
-    return { name, code, population, size, density };
-}
+// function createData(name: string, code: string, population: number, size: number): Data {
+//     const density = population / size;
+//     return { name, code, population, size, density };
+// }
 
 const rows = [
-    createData('India', 'IN', 1324171354, 3287263),
-    createData('China', 'CN', 1403500365, 9596961),
-    createData('Italy', 'IT', 60483973, 301340),
-    createData('United States', 'US', 327167434, 9833520),
-    createData('Canada', 'CA', 37602103, 9984670),
-    createData('Australia', 'AU', 25475400, 7692024),
-    createData('Germany', 'DE', 83019200, 357578),
-    createData('Ireland', 'IE', 4857000, 70273),
-    createData('Mexico', 'MX', 126577691, 1972550),
-    createData('Japan', 'JP', 126317000, 377973),
-    createData('France', 'FR', 67022000, 640679),
-    createData('United Kingdom', 'GB', 67545757, 242495),
-    createData('Russia', 'RU', 146793744, 17098246),
-    createData('Nigeria', 'NG', 200962417, 923768),
-    createData('Brazil', 'BR', 210147125, 8515767),
+    { name: 'India', code: 'IN', population: 1324171354, size: 3287263 },
+    { name: 'India', code: 'IN', population: 1324171354, size: 3287263 },
+    { name: 'India', code: 'IN', population: 1324171354, size: 3287263 },
+    { name: 'India', code: 'IN', population: 1324171354, size: 3287263 },
+    { name: 'India', code: 'IN', population: 1324171354, size: 3287263 },
+    { name: 'India', code: 'IN', population: 1324171354, size: 3287263 },
+    { name: 'India', code: 'IN', population: 1324171354, size: 3287263 },
+    { name: 'India', code: 'IN', population: 1324171354, size: 3287263 },
+    { name: 'India', code: 'IN', population: 1324171354, size: 3287263 },
+    { name: 'India', code: 'IN', population: 1324171354, size: 3287263 },
+    { name: 'India', code: 'IN', population: 1324171354, size: 3287263 },
+    { name: 'India', code: 'IN', population: 1324171354, size: 3287263 },
+    { name: 'India', code: 'IN', population: 1324171354, size: 3287263 },
+    { name: 'India', code: 'IN', population: 1324171354, size: 3287263 },
+    { name: 'India', code: 'IN', population: 1324171354, size: 3287263 },
+    { name: 'India', code: 'IN', population: 1324171354, size: 3287263 },
+    { name: 'India', code: 'IN', population: 1324171354, size: 3287263 },
+    { name: 'India', code: 'IN', population: 1324171354, size: 3287263 },
+    { name: 'India', code: 'IN', population: 1324171354, size: 3287263 },
+    { name: 'India', code: 'IN', population: 1324171354, size: 3287263 },
+    { name: 'India', code: 'IN', population: 1324171354, size: 3287263 },
+    { name: 'India', code: 'IN', population: 1324171354, size: 3287263 },
+    { name: 'India', code: 'IN', population: 1324171354, size: 3287263 },
+    { name: 'India', code: 'IN', population: 1324171354, size: 3287263 },
+    { name: 'India', code: 'IN', population: 1324171354, size: 3287263 },
+
 ];
 
 const useStyles = makeStyles({
